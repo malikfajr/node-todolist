0b3444cd02a3fc08f68ba4d833db926504e0fecd
malik <malilfajar17@gmail.com>
2023-11-23 21:41:41 +0700

    Add morgan middleware and update linting script

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
+const PORT = require('./src/config/index').port;
 
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +17,6 @@ app.use((req, res) => {
   res.status(404).send({ message: `Route${req.url} Not found.` });
 });
 
-app.listen(3030, () => {
+app.listen(PORT, () => {
   console.log('Server running on port 3030');
 });
